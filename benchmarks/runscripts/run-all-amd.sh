9a4724be79134fd497a3aa0a41480484e3c7a20a
koparasy <parasyris1@llnl.gov>
2024-12-03 21:00:35 -0800

    Changes to link with new naming

--- benchmarks/runscripts/run-all-amd.sh
+++ benchmarks/runscripts/run-all-amd.sh
@@ -8,9 +8,9 @@ python driver.py -g "hip/*" --compiler ${CXX} -j $PWD/../build-amd -m amd -x dir
 # Profiler runs.
 python driver.py -g "hip/*" --compiler ${CXX} -j $PWD/../build-amd -m amd -x profiler
 
-# Profiler with metrics runs.
-python driver.py -g "hip/adam" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
-python driver.py -g "hip/feynman-kac" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
-python driver.py -g "hip/wsm5" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
-python driver.py -g "hip/rsbench" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
-python driver.py -g "hip/sw4ck" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
+## Profiler with metrics runs.
+#python driver.py -g "hip/adam" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
+#python driver.py -g "hip/feynman-kac" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
+#python driver.py -g "hip/wsm5" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
+#python driver.py -g "hip/rsbench" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
+#python driver.py -g "hip/sw4ck" --compiler ${CXX} -j $PWD/../build-amd -m amd -x metrics
