d7762beff20dfbb9064a377f8deeaaf0b87c2606
David Beckingsale <davidbeckingsale@gmail.com>
2024-12-05 10:35:14 -0800

    Add CHECKs to lambda test

--- tests/cpu/lambda.cpp
+++ tests/cpu/lambda.cpp
@@ -24,13 +24,21 @@ int main(int argc, char **argv) {
     y[i] = 0.0;
   }
 
+  std::cout << y[1] << std::endl;
+
   run([=, N=proteus::jit_variable(N), a=proteus::jit_variable(a), b=proteus::jit_variable(b)] ()  __attribute__((annotate("jit"))) {
     for (std::size_t i{0}; i < N; ++i) {
       y[i] += a * b * x[i];
     }
   });
-  
+
+  std::cout << y[1] << std::endl;
 
   free(x);
   free(y);
 }
+
+// CHECK: 0
+// CHECK: 1.46382
+// CHECK: JitCache hits 0 total 1
+// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
