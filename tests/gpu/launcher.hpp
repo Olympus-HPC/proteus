eb1e51c501d738e4860a82884ec99a8b74760f0b
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2024-12-21 02:30:41 -0800

    Set launcher always_inline
    - Match the RAJA-based test case
    - Make caching result consistent

--- tests/gpu/launcher.hpp
+++ tests/gpu/launcher.hpp
@@ -9,7 +9,7 @@ __global__ __attribute__((annotate("jit"))) void kernel(LB lb) {
   lb();
 }
 
-template <typename T> gpuError_t launcher(T lb) {
+template <typename T> gpuError_t __attribute__((always_inline)) launcher(T lb) {
   auto func = reinterpret_cast<const void *>(&kernel<T>);
   void *args[] = {(void *)&lb};
   return gpuLaunchKernel(func, 1, 1, args, 0, 0);
