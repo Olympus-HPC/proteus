111db90eec8bd636776cce80197d30075257f555
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-01-08 11:14:33 -0800

    Use printout time tracing

--- lib/TimeTracing.hpp
+++ lib/TimeTracing.hpp
@@ -12,6 +12,8 @@
 #define PROTEUS_TIME_TRACING_HPP
 
 #include "llvm/Support/TimeProfiler.h"
+#include <chrono>
+#include <iostream>
 
 namespace proteus {
 
@@ -29,8 +31,24 @@ struct TimeTracerRAII {
   }
 };
 
+struct TimeTracerPrintout {
+  TimeTracerPrintout(StringRef Tag)
+      : Tag(Tag), Tstart(std::chrono::high_resolution_clock::now()) {}
+
+  ~TimeTracerPrintout() {
+    auto Tend = std::chrono::high_resolution_clock::now();
+    auto Duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(Tend - Tstart)
+            .count();
+    std::cout << "[PROTEUS TRACE] " << Tag << " => " << Duration << " us\n";
+  }
+
+  std::chrono::high_resolution_clock::time_point Tstart;
+  std::string Tag;
+};
+
 #if ENABLE_TIME_TRACING
-#define TIMESCOPE(x) TimeTraceScope T(x);
+#define TIMESCOPE(x) TimeTracerPrintout T(x);
 #else
 #define TIMESCOPE(x)
 #endif
