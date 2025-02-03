730c989a963376ccabf98684770597c837a0a59f
john bowen <bowen36@llnl.gov>
2025-02-03 11:55:43 -0800

    syntax

--- scripts/gitlab/ci-run-benchmarks.sh
+++ scripts/gitlab/ci-run-benchmarks.sh
@@ -46,7 +46,7 @@ BENCHMARKS_TOML=""
 if [ "${CI_MACHINE}" == "lassen" ]; then
   if [[ "${COMMENTS_BODY}" == *"/run-benchmarks-rajaperf"* ]]; then
     echo "RAJAPerf benchmarks can only run on tioga.  Exiting"
-    exit(0)
+    exit 0
   fi
   ml load cuda/12.2.2
 
