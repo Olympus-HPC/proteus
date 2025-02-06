f8f1de4b4ed543ce456e179f372f9c924cba89a7
john bowen <bowen36@llnl.gov>
2025-02-06 10:47:18 -0800

    try debugging script

--- scripts/gitlab/ci-run-benchmarks.sh
+++ scripts/gitlab/ci-run-benchmarks.sh
@@ -107,7 +107,8 @@ git submodule update --init --recursive
 
 python driver.py -t ${BENCHMARKS_TOML} \
   -c ${PROTEUS_CC} -j ${PROTEUS_INSTALL_PATH} -x aot -p direct -m ${MACHINE} -r 1
-
+echo "RAN AOT"
+exit 0
 python driver.py -t ${BENCHMARKS_TOML} \
   -c ${PROTEUS_CC} -j ${PROTEUS_INSTALL_PATH} -x aot -p profiler -m ${MACHINE} -r 1
 
@@ -157,7 +158,7 @@ if [[ "${COMMENTS_BODY}" == *"/run-benchmarks-hecbench"* ]]; then
 fi
 
 echo "End run benchmarks..."
-exit(0)
+
 python vis/plot-bar-end2end-speedup.py --dir results --plot-dir plots -m ${MACHINE} -f png --plot-title ${CI_MACHINE}
 python vis/plot-bar-end2end-speedup-noopt.py --dir results --plot-dir plots -m ${MACHINE} -f png --plot-title ${CI_MACHINE}
 python vis/plot-bar-compilation-slowdown.py  --dir results --plot-dir plots -m ${MACHINE} -f png --plot-title ${CI_MACHINE}
