52be49ced7d65a098be06521bbb70329bd705579
Tom Stitt <tomstitt@fastmail.com>
2025-02-14 07:16:54 -0800

    Fix type matching for lambdas with arguments  (#152)
    * Loosen demangled name matching strictness to handle lambdas with arguments
    
    * Add test

--- tests/gpu/lambda.cpp
+++ tests/gpu/lambda.cpp
@@ -17,9 +17,24 @@ __global__ __attribute__((annotate("jit"))) void kernel(T LB) {
     LB();
 }
 
+template <typename T>
+__global__ __attribute__((annotate("jit"))) void kernel(int n, T LB) {
+  std::size_t i = blockDim.x * blockIdx.x + threadIdx.x;
+  if (i < n)
+    LB(i);
+}
+
 template <typename T> void register_run(T &&LB) {
   proteus::register_lambda(LB);
   kernel<<<1, 1>>>(LB);
+  gpuErrCheck(gpuDeviceSynchronize());
+}
+
+template <typename T> void register_run(int n, T &&LB) {
+  proteus::register_lambda(LB);
+  constexpr int block_size = 256;
+  const int num_blocks = (n + block_size - 1) / block_size;
+  kernel<<<num_blocks, block_size>>>(n, LB);
   gpuErrCheck(gpuDeviceSynchronize());
 }
 
@@ -46,6 +61,12 @@ int main(int argc, char **argv) {
 
   std::cout << "x[0] = " << x[0] << "\n";
 
+  register_run(
+      2, [ =, c = proteus::jit_variable(c) ] __device__
+      __attribute__((annotate("jit"))) (int i) { x[i] = c; });
+
+  std::cout << "x[0:1] = " << x[0] << "," << x[1] << "\n";
+
   run(proteus::register_lambda([
     =, a = proteus::jit_variable(a), b = proteus::jit_variable(b)
   ] __device__ __attribute__((annotate("jit"))) () { x[0] = a + b; }));
@@ -56,9 +77,10 @@ int main(int argc, char **argv) {
 }
 
 // CHECK: x[0] = 3.14
+// CHECK: x[0:1] = 4.56,4.56
 // CHECK: x[0] = 4.37
 // CHECK: x[0] = 6.56
-// CHECK: JitCache hits 0 total 3
+// CHECK: JitCache hits 0 total 4
 // CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
-// CHECK-FIRST: JitStorageCache hits 0 total 3
-// CHECK-SECOND: JitStorageCache hits 3 total 3
+// CHECK-FIRST: JitStorageCache hits 0 total 4
+// CHECK-SECOND: JitStorageCache hits 4 total 4
