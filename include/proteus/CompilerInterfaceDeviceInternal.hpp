544bb80b733c24df8fc87216d842413b88baef9e
David Beckingsale <davidbeckingsale@gmail.com>
2025-03-18 10:17:13 -0700

    Add environment variable check for Proteus disable flag in JIT kernel launch

--- include/proteus/CompilerInterfaceDeviceInternal.hpp
+++ include/proteus/CompilerInterfaceDeviceInternal.hpp
@@ -10,6 +10,15 @@ inline auto __jit_launch_kernel_internal(void *Kernel, dim3 GridDim,
   using namespace llvm;
   using namespace proteus;
 
+  static const bool IsProteusDisabledEnvVar =
+      getEnvOrDefaultBool("PROTEUS_DISABLE", false);
+
+  if (IsProteusDisabledEnvVar) {
+    return proteus::launchKernelDirect(
+        Kernel, GridDim, BlockDim, KernelArgs, ShmemSize,
+        static_cast<typename JitDeviceImplT::DeviceStream_t>(Stream));
+  }
+
   auto &Jit = JitDeviceImplT::instance();
   if (Jit.isProteusDisabled()) {
     return proteus::launchKernelDirect(
