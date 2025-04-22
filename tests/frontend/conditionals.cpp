610d5532b7b7267362ac315678ee496d52028d49
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-04-21 18:02:32 -0700

    Cleanup code

--- tests/frontend/conditionals.cpp
+++ tests/frontend/conditionals.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
   F.endFunction();
   J.print();
 
-  double (*Func)() = (double (*)())J.compile();
+  auto *Func = (double (*)())J.compile();
   double Ret = Func();
   std::cout << "Ret " << Ret << "\n";
 
