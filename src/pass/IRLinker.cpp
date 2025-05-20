4cb8768218a2835df0e681c92b32940bc14eb699
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-05-20 11:43:21 -0700

    Fixup add missing file

---
+++ src/pass/IRLinker.cpp
@@ -0,0 +1,54 @@
+#include "llvm-c/Linker.h"
+#include <llvm/ADT/SmallPtrSet.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/ADT/StringSet.h>
+#include <llvm/ADT/Twine.h>
+#include <llvm/Demangle/Demangle.h>
+#include <llvm/IR/CallingConv.h>
+#include <llvm/IR/DataLayout.h>
+#include <llvm/IR/DebugInfo.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/GlobalValue.h>
+#include <llvm/IR/GlobalVariable.h>
+#include <llvm/IR/InstrTypes.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/Mangler.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Linker/Linker.h>
+#include <llvm/Object/ELF.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/PassPlugin.h>
+#include <llvm/Support/Debug.h>
+#include <llvm/Support/Error.h>
+#include <llvm/Support/ErrorHandling.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/FileSystem/UniqueID.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/MemoryBufferRef.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include "llvm/LTO/legacy/LTOModule.h"
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/Bitcode/BitcodeWriter.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/LTO/LTO.h>
+#include <llvm/LTO/legacy/LTOCodeGenerator.h>
+
+#include "proteus/Error.h"
+
+using namespace llvm;
+
+void linkModules(Module &Dest,
+                 SmallVector<std::unique_ptr<Module>> &LinkedModules) {
+  dbgs() << "IMBA LINKER!\n";
+  Linker IRLinker(Dest);
+  for (auto &Mod : LinkedModules) {
+    // if (LLVMLinkModules2(wrap(&Dest), wrap(Mod.get())))
+    if (IRLinker.linkInModule(std::move(Mod)))
+      PROTEUS_FATAL_ERROR("Linking failed");
+  }
+}
