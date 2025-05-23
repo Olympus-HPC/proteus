1733ecf45728c39c9a19ecb8dc6cf7161b9f7187
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-05-23 16:16:01 -0700

    Fix cloning for GlobalAlias
    - Add test too

---
+++ tests/gpu/alias.cpp
@@ -0,0 +1,44 @@
+// clang-format off
+// RUN: rm -rf .proteus
+// RUN: ./alias.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-FIRST
+// Second run uses the object cache.
+// RUN: ./alias.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-SECOND
+// RUN: rm -rf .proteus
+// clang-format on
+
+#include "gpu_common.h"
+
+#ifdef __HIP_DEVICE_COMPILE__
+// 1) Forward‐declare the real device function
+extern "C" __device__ __attribute__((used)) void foo(void *ptr, int v);
+
+// 2) Create the alias (must refer to the mangled/assembler name)
+extern "C" __device__ __attribute__((used)) void foo_alias(void *ptr, int v)
+    __attribute__((alias("foo")));
+
+// 3) Define the aliasee
+extern "C" __device__ __attribute__((used)) void foo(void *ptr, int v) {
+  printf("foo\n");
+}
+#endif
+
+// A trivial kernel that forces emission of both symbols
+__global__ __attribute__((annotate("jit"))) void kernel() {
+#ifdef __HIP_DEVICE_COMPILE__
+  printf("Kernel\n");
+  foo_alias(nullptr, 42);
+#endif
+}
+
+int main() {
+  kernel<<<1, 1>>>();
+  gpuErrCheck(gpuDeviceSynchronize());
+  return 0;
+}
+
+// CHECK: Kernel
+// CHECK: foo
+// CHECK: JitCache hits 0 total 1
+// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
+// CHECK-FIRST: JitStorageCache hits 0 total 1
+// CHECK-SECOND: JitStorageCache hits 1 total 1
