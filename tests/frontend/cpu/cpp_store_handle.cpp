7740302f7d5c488551a6d8d2415063582f550de7
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-08-28 09:11:20 -0700

    [cpp-frontend] Make function, kernel handles public (#264)
    - Allows the user to store and re-use

---
+++ tests/frontend/cpu/cpp_store_handle.cpp
@@ -0,0 +1,35 @@
+// clang-format off
+// RUN: rm -rf .proteus
+// RUN: ./cpp_store_handle | %FILECHECK %s --check-prefixes=CHECK
+// RUN: rm -rf .proteus
+// clang-format on
+
+#include "proteus/CppJitModule.hpp"
+
+using namespace proteus;
+
+struct FuncStore {
+  CppJitModule::FunctionHandle<void(int)> Func;
+};
+
+void launcher(FuncStore &FS) { FS.Func.run(42); }
+
+int main() {
+  const char *Code = R"cpp(
+    #include <cstdio>
+
+    extern "C" void foo(int a) {
+        printf("Function %d\n", a);
+    }
+   )cpp";
+
+  CppJitModule CJM{"host", Code};
+  auto Func = CJM.getFunction<void(int)>("foo");
+  Func.run(42);
+  FuncStore FS{Func};
+  launcher(FS);
+}
+
+// clang-format off
+// CHECK: Function 42
+// CHECK: Function 42
