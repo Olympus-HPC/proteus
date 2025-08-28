38c2854d51f8653b20f94004105073e03192dcbb
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-08-28 14:22:06 -0700

    Fixup: add g++ to the cmake of the test app

--- tests/integration/hip/cmake-proteusFrontend-gcc/ci-build-and-run.sh
+++ tests/integration/hip/cmake-proteusFrontend-gcc/ci-build-and-run.sh
@@ -2,6 +2,7 @@ set -e
 
 cmake -S ${CI_PROJECT_DIR} -B build-proteus \
     -DLLVM_INSTALL_DIR="${LLVM_INSTALL_DIR}" \
+    -DCMAKE_CXX_COMPILER="gcc" \
     -DCMAKE_CXX_COMPILER="g++" \
     -DCMAKE_INSTALL_PREFIX=${PWD}/install-proteus \
     -DPROTEUS_ENABLE_HIP=on \
@@ -13,7 +14,7 @@ make -j install
 popd
 
 cmake -S ${TEST_DIR} -B build \
-    -DLLVM_INSTALL_DIR="${LLVM_INSTALL_DIR}" \
+    -DCMAKE_CXX_COMPILER="g++" \
     -Dproteus_DIR="${PWD}/install-proteus" \
     -DCMAKE_INSTALL_PREFIX="${PWD}/install"
 pushd build
