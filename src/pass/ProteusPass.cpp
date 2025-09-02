5e97f380ec239c5930ad7add64e1131dfe759156
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-09-02 08:03:37 -0700

    Fix linking bug with proteus and non-proteus objects
    - Do not change linkage and visibility in the original module's symbols
    - Remove duplicate check for unresolved symbols in the LLVM IR
    - Link consumer tests with `-rdynamic` (as we document)

--- src/pass/ProteusPass.cpp
+++ src/pass/ProteusPass.cpp
@@ -244,40 +244,6 @@ private:
     runCleanupPassPipeline(*JitMod);
     runOptimizationPassPipeline(*JitMod);
 
-    // Update linkage and visibility in the original module only for
-    // globals included in the JIT module required for external
-    // linking.
-    for (auto &GVar : M.globals()) {
-      [[maybe_unused]] auto PrintGVarInfo = [](auto &GVar) {
-        Logger::logs("proteus-pass") << "=== GVar\n";
-        Logger::logs("proteus-pass") << GVar.getName() << "\n";
-        Logger::logs("proteus-pass") << "Linkage " << GVar.getLinkage() << "\n";
-        Logger::logs("proteus-pass")
-            << "Visibility " << GVar.getVisibility() << "\n";
-        Logger::logs("proteus-pass") << "=== End GV\n";
-      };
-
-      if (VMap[&GVar]) {
-        DEBUG(PrintGVarInfo(GVar));
-
-        if (GVar.isConstant())
-          continue;
-
-        if (GVar.getName() == "llvm.global_ctors") {
-          DEBUG(Logger::logs("proteus-pass") << "Skip llvm.global_ctors");
-          continue;
-        }
-
-        if (GVar.hasAvailableExternallyLinkage()) {
-          DEBUG(Logger::logs("proteus-pass") << "Skip available externally");
-          continue;
-        }
-
-        GVar.setLinkage(GlobalValue::ExternalLinkage);
-        GVar.setVisibility(GlobalValue::VisibilityTypes::DefaultVisibility);
-      }
-    }
-
     // TODO: Do we want to keep debug info to use for GDB/LLDB
     // interfaces for debugging jitted code?
     StripDebugInfo(*JitMod);
