b1fd0ad1d00cf48a172ae756167fa84ebe5e23c5
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-09-04 10:17:49 -0700

    Fixup: harden rm for proteus cache dirs using quotes

--- tests/cpu/custom_pipeline.cpp
+++ tests/cpu/custom_pipeline.cpp
@@ -1,17 +1,17 @@
 // clang-format off
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_CODEGEN=serial PROTEUS_TRACE_OUTPUT=1 %build/custom_pipeline | %FILECHECK %s --check-prefixes=CHECK
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_TRACE_OUTPUT=1 PROTEUS_CODEGEN=serial PROTEUS_TRACE_OUTPUT=1 PROTEUS_OPT_PIPELINE='default<O3>' %build/custom_pipeline | %FILECHECK %s --check-prefixes=CHECK,CHECK3
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_TRACE_OUTPUT=1 PROTEUS_CODEGEN=serial PROTEUS_TRACE_OUTPUT=1 PROTEUS_OPT_PIPELINE='default<O2>' %build/custom_pipeline | %FILECHECK %s --check-prefixes=CHECK,CHECK2
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_TRACE_OUTPUT=1 PROTEUS_CODEGEN=serial PROTEUS_TRACE_OUTPUT=1 PROTEUS_OPT_PIPELINE='default<O1>' %build/custom_pipeline | %FILECHECK %s --check-prefixes=CHECK,CHECK1
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_TRACE_OUTPUT=1 PROTEUS_CODEGEN=serial PROTEUS_TRACE_OUTPUT=1 PROTEUS_OPT_PIPELINE='default<Os>' %build/custom_pipeline | %FILECHECK %s --check-prefixes=CHECK,CHECKs
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_TRACE_OUTPUT=1 PROTEUS_CODEGEN=serial PROTEUS_TRACE_OUTPUT=1 PROTEUS_OPT_PIPELINE='default<Oz>' %build/custom_pipeline | %FILECHECK %s --check-prefixes=CHECK,CHECKz
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // clang-format on
 
 #include <cstddef>
--- tests/cpu/types_api.cpp
+++ tests/cpu/types_api.cpp
@@ -1,9 +1,9 @@
 // clang-format off
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_TRACE_OUTPUT=1 %build/types_api | %FILECHECK %s --check-prefixes=CHECK,CHECK-FIRST
 // Second run uses the object cache.
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" %build/types_api | %FILECHECK %s --check-prefixes=CHECK,CHECK-SECOND
-// RUN: rm -rf %t.$$.proteus
+// RUN: rm -rf "%t.$$.proteus"
 // clang-format on
 
 #include <cstdlib>
