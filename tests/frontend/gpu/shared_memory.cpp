aba534366bdccc9d39e01276f1b5b0dfb3729d29
Zane Fink <finkzane@gmail.com>
2025-09-10 14:25:50 -0700

    Update cache dir

--- tests/frontend/gpu/shared_memory.cpp
+++ tests/frontend/gpu/shared_memory.cpp
@@ -1,10 +1,10 @@
 // tests/frontend/gpu/shared_memory.cpp
 // clang-format off
-// RUN: rm -rf .proteus
-// RUN: ./shared_memory.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-FIRST
+// RUN: rm -rf "%t.$$.proteus"
+// RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" %build/shared_memory.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-FIRST
 // Second run uses the object cache.
-// RUN: ./shared_memory.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-SECOND
-// RUN: rm -rf .proteus
+// RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" %build/shared_memory.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-SECOND
+// RUN: rm -rf "%t.$$.proteus"
 // clang-format on
 
 #include <proteus/Frontend/Builtins.hpp>
