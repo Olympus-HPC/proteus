25beadaf7a808a482b18b464fd021e95a1ab48ab
koparasy <parasyris1@llnl.gov>
2025-09-19 15:35:54 -0700

    Fix url

--- tests/integration/hip/mneme-install/ci-build-and-run.sh
+++ tests/integration/hip/mneme-install/ci-build-and-run.sh
@@ -4,5 +4,5 @@ ml load python/3.9
 
 python -m venv $SYS_TYPE
 source $SYS_TYPE/bin/activate
-PROTEUS_SRC=${CI_PROJECT_DIR} pip install https://github.com/Olympus-HPC/Mneme.git
+PROTEUS_SRC=${CI_PROJECT_DIR} pip install git+https://github.com/Olympus-HPC/Mneme.git
 
