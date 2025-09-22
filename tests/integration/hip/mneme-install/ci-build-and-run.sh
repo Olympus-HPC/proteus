59c1938ce9cd037dc5f1fbdafc54c8e9107cdcee
koparasy <parasyris1@llnl.gov>
2025-09-22 06:59:31 -0700

    Install with verbose mode

--- tests/integration/hip/mneme-install/ci-build-and-run.sh
+++ tests/integration/hip/mneme-install/ci-build-and-run.sh
@@ -2,7 +2,8 @@ set -e
 
 ml load python/3.9
 
+echo "DIRS are under"
 python -m venv $SYS_TYPE
 source $SYS_TYPE/bin/activate
-LLVM_INSTALL_DIR=${ROCM_PATH} PROTEUS_SRC=${CI_PROJECT_DIR} pip install git+https://github.com/Olympus-HPC/Mneme.git
+LLVM_INSTALL_DIR=${ROCM_PATH} PROTEUS_SRC=${CI_PROJECT_DIR} pip install -v git+https://github.com/Olympus-HPC/Mneme.git
 
