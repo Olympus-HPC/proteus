c18d0ac3600b48ebb9b8c9c15c2e0d6e41dfd3d5
Zane Fink <finkzane@gmail.com>
2025-09-23 13:49:50 -0700

    Update checks

--- tests/frontend/gpu/for_early_ret.cpp
+++ tests/frontend/gpu/for_early_ret.cpp
@@ -83,7 +83,7 @@ int main() {
 // CHECK-NEXT: X[7] = 1
 // CHECK-NEXT: X[8] = 1
 // CHECK-NEXT: X[9] = 1
-// CHECK: JitCache hits 0 total 2
-// CHECK-COUNT-2: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
+// CHECK: JitCache hits 0 total 1
+// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
 // CHECK-FIRST: JitStorageCache hits 0 total 1
 // CHECK-SECOND: JitStorageCache hits 1 total 1
--- tests/frontend/gpu/if_early_ret.cpp
+++ tests/frontend/gpu/if_early_ret.cpp
@@ -70,6 +70,6 @@ int main() {
 // CHECK: X 0
 // CHECK-NEXT: X 1
 // CHECK: JitCache hits 0 total 1
-// CHECK-COUNT-1: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
+// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
 // CHECK-FIRST: JitStorageCache hits 0 total 1
 // CHECK-SECOND: JitStorageCache hits 1 total 1
