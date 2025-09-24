65a4974ce1ff3e3934d867d9526f272126578fb4
Zane Fink <finkzane@gmail.com>
2025-09-24 15:02:43 -0700

    Everybody do the clang-format

--- tests/frontend/gpu/launch_bounds.cpp
+++ tests/frontend/gpu/launch_bounds.cpp
@@ -26,9 +26,7 @@ int main() {
   auto &F = KernelHandle.F;
 
   F.beginFunction();
-  {
-    F.ret();
-  }
+  { F.ret(); }
   F.endFunction();
 
   KernelHandle.setLaunchBounds(MaxThreadsPerBlock, MinBlocksPerSM);
