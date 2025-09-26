a914c604745a87dc22522f167288e4477921cad3
koparasy <parasyris1@llnl.gov>
2025-09-26 14:11:16 -0700

    Add lang name and use it on lit

--- tests/gpu/kernel_tuning.cpp
+++ tests/gpu/kernel_tuning.cpp
@@ -1,6 +1,6 @@
 // clang-format off
 // RUN: rm -rf "%t.$$.proteus"
-// RUN: PROTEUS_TUNED_KERNELS=%S/tuned_config.json PROTEUS_TRACE_OUTPUT=1 %build/kernel_tuning.%ext | %FILECHECK %s --check-prefixes=CHECK
+// RUN: PROTEUS_TUNED_KERNELS=%S/tuned_config.json PROTEUS_TRACE_OUTPUT=1 %build/kernel_tuning.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-%device_lang
 // RUN: rm -rf "%t.$$.proteus"
 // clang-format on
 
@@ -28,7 +28,9 @@ int main() {
   proteus::init();
 
   foo<<<1, 1>>>();
+  gpuErrCheck(gpuDeviceSynchronize());
   bar<<<1, 1>>>();
+  gpuErrCheck(gpuDeviceSynchronize());
   baz<<<1, 1>>>();
   gpuErrCheck(gpuDeviceSynchronize());
 
@@ -43,12 +45,13 @@ int main() {
 // CHECK: [KernelConfig] ID:bar CG:RTC SA:1 LB:1 SD:1 SDA:1 OL:3 CGL:2
 // CHECK: [LaunchBoundSpec] BlockSize 1
 // CHECK: Hello from bar
-// CHECK: [KernelConfig] ID:baz CG:RTC SA:1 LB:1 SD:1 SDA:0 OL:3 CGL:3
+// CHECK-CUDA: [KernelConfig] ID:baz CG:RTC SA:1 LB:1 SD:1 SDA:0 OL:3 CGL:3
+// CHECK-HIP: [KernelConfig] ID:baz CG:RTC SA:1 LB:1 SD:1 SDA:1 OL:3 CGL:3
 // CHECK: [LaunchBoundSpec] BlockSize 1
 // CHECK: Hello from baz
 // CHECK: JitCache hits 0 total 3
-// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0 FnName baz
-// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0 FnName foo
-// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0 FnName bar
+// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
+// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
+// CHECK: HashValue {{[0-9]+}} NumExecs 1 NumHits 0
 // CHECK: JitStorageCache hits 0 total 3
 
