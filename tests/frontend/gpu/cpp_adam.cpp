c372f217f0f0b4a7f0a788a5b2978fa5c9b64c4c
Zane Fink <finkzane@gmail.com>
2025-10-02 15:18:22 -0700

    Force trace output in first run

--- tests/frontend/gpu/cpp_adam.cpp
+++ tests/frontend/gpu/cpp_adam.cpp
@@ -2,7 +2,7 @@
 
 // clang-format off
 // RUN: rm -rf "%t.$$.proteus"
-// RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" %build/cpp_adam.%ext 10000 200 100 | %FILECHECK %s --check-prefixes=CHECK,CHECK-FIRST
+// RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_TRACE_OUTPUT=1 %build/cpp_adam.%ext 10000 200 100 | %FILECHECK %s --check-prefixes=CHECK,CHECK-FIRST
 // Second run uses the object cache.
 // RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" %build/cpp_adam.%ext 10000 200 100 | %FILECHECK %s --check-prefixes=CHECK,CHECK-SECOND
 // RUN: rm -rf "%t.$$.proteus"
