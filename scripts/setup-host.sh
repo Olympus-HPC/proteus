5283da1ba3f4d6a22fb445187f44ac414e9b26fa
Zane Fink <finkzane@gmail.com>
2025-10-14 11:09:35 -0700

    accidental changes to setup-host

--- scripts/setup-host.sh
+++ scripts/setup-host.sh
@@ -6,22 +6,19 @@ if [ $# -lt 1 ]; then
     return 0
 fi
 
-#ml load clang/${CLANG_VERSION}
+ml load clang/${CLANG_VERSION}
 
 LLVM_INSTALL_DIR=$(llvm-config --prefix)
 
 mkdir build-host-clang_${CLANG_VERSION}
 pushd build-host-clang_${CLANG_VERSION}
-export CMAKE_PREFIX_PATH="$CONDA_PREFIX:${CMAKE_PREFIX_PATH}"
 
 cmake .. \
 -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
 -DCMAKE_C_COMPILER=${LLVM_INSTALL_DIR}/bin/clang \
 -DCMAKE_CXX_COMPILER=${LLVM_INSTALL_DIR}/bin/clang++ \
 -DCMAKE_INSTALL_PREFIX=../install-rocm-${CLANG_VERSION} \
--DCMAKE_BUILD_TYPE=RelWithDebInfo \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=on \
--DPROTEUS_LINK_SHARED_LLVM=on \
 "${@:2}"
 
 popd
