ee93ad314b927b9c0f77bc7a2c8572bfe5c48c66
Zane Fink <finkzane@gmail.com>
2025-10-17 14:29:40 -0700

    PJ-DSL: While Loop (#292)

--- src/lib/Frontend/Func.cpp
+++ src/lib/Frontend/Func.cpp
@@ -175,4 +175,21 @@ void FuncBase::endFor() {
 
   IRB.restoreIP(IP);
 }
+
+void FuncBase::endWhile() {
+  if (Scopes.empty())
+    PROTEUS_FATAL_ERROR("Expected WHILE scope");
+
+  Scope S = Scopes.back();
+  if (S.Kind != ScopeKind::WHILE)
+    PROTEUS_FATAL_ERROR("Syntax error, expected WHILE end scope but "
+                        "found unterminated scope " +
+                        toString(S.Kind) + " @ " + S.File + ":" +
+                        std::to_string(S.Line));
+
+  IP = S.ContIP;
+  Scopes.pop_back();
+
+  IRB.restoreIP(IP);
+}
 } // namespace proteus
