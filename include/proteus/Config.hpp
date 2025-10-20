2427bc0a905ce36665a28750816658001d2d7cd0
Zane Fink <finkzane@gmail.com>
2025-10-20 14:24:14 -0700

    Move config checks into source file

--- include/proteus/Config.hpp
+++ include/proteus/Config.hpp
@@ -128,18 +128,9 @@ inline KernelCloneOption getEnvOrDefaultKC(const char *VarName,
 }
 
 class CodeGenerationConfig {
-  static constexpr bool DefaultSpecializeDimsRange = !PROTEUS_ENABLE_CUDA;
+  static const bool DefaultSpecializeDimsRange;
 
-  static CodegenOption getCodeGen(CodegenOption ProteusCodegen) {
-    constexpr bool SupportOnlyRTC = PROTEUS_ENABLE_CUDA;
-    if (SupportOnlyRTC && ProteusCodegen != CodegenOption::RTC) {
-      Logger::outs("proteus") << "Warning: Proteus supports only RTC in the "
-                                 "current build system configuration, "
-                                 "defaulting Codegen to RTC\n";
-      ProteusCodegen = CodegenOption::RTC;
-    }
-    return ProteusCodegen;
-  }
+  static CodegenOption getCodeGen(CodegenOption ProteusCodegen);
 
   std::optional<const std::string> ProteusOptPipeline;
   CodegenOption ProteusCodegen;
@@ -158,78 +149,24 @@ class CodeGenerationConfig {
                        bool ProteusSpecializeDims,
                        bool ProteusSpecializeDimsRange, char ProteusOptLevel,
                        int ProteusCodeGenOptLevel, int TunedMaxThreads = -1,
-                       int MinBlocksPerSM = 0)
-      : ProteusOptPipeline(ProteusOptPipeline), ProteusCodegen(ProteusCodegen),
-        ProteusSpecializeArgs(ProteusSpecializeArgs),
-        ProteusSpecializeLaunchBounds(ProteusSpecializeLaunchBounds),
-        ProteusSpecializeDims(ProteusSpecializeDims),
-        ProteusSpecializeDimsRange(ProteusSpecializeDimsRange),
-        ProteusOptLevel(ProteusOptLevel),
-        ProteusCodeGenOptLevel(ProteusCodeGenOptLevel),
-        TunedMaxThreads(TunedMaxThreads), MinBlocksPerSM(MinBlocksPerSM) {}
+                       int MinBlocksPerSM = 0);
 
 public:
-  static CodeGenerationConfig createFromEnv() {
-    return CodeGenerationConfig(
-        getEnvOrDefaultString("PROTEUS_OPT_PIPELINE"),
-        getCodeGen(getEnvOrDefaultCG("PROTEUS_CODEGEN", CodegenOption::RTC)),
-        getEnvOrDefaultBool("PROTEUS_SPECIALIZE_ARGS", true),
-        getEnvOrDefaultBool("PROTEUS_SPECIALIZE_LAUNCH_BOUNDS", true),
-        getEnvOrDefaultBool("PROTEUS_SPECIALIZE_DIMS", true),
-        getEnvOrDefaultBool("PROTEUS_SPECIALIZE_DIMS_RANGE",
-                            DefaultSpecializeDimsRange),
-        getEnvOrDefaultChar("PROTEUS_OPT_LEVEL", '3'),
-        getEnvOrDefaultInt("PROTEUS_CODEGEN_OPT_LEVEL", 3));
-  }
+  static CodeGenerationConfig createFromEnv();
 
   static CodeGenerationConfig
-  createFromJSONEntry(const llvm::json::Object &Config) {
-    auto Pipeline = Config.getString("Pipeline");
-    std::optional<std::string> ProteusPipeline;
-    if (Pipeline)
-      ProteusPipeline = Pipeline.value().str();
+  createFromJSONEntry(const llvm::json::Object &Config);
 
-    return CodeGenerationConfig(
-        ProteusPipeline,
-        getCodeGen(
-            strToCG(getDefaultValueFromOptional(Config.getString("CodeGen"),
-                                                llvm::StringRef("rtc"))
-                        .str())),
-        getDefaultValueFromOptional(Config.getBoolean("SpecializeArgs"), true),
-        getDefaultValueFromOptional(Config.getBoolean("LaunchBounds"), true),
-        getDefaultValueFromOptional(Config.getBoolean("SpecializeDims"), true),
-        getDefaultValueFromOptional(Config.getBoolean("SpecializeDimsRange"),
-                                    DefaultSpecializeDimsRange),
-        getDefaultValueFromOptional(Config.getString("OptLevel"),
-                                    llvm::StringRef("3"))[0],
-        getDefaultValueFromOptional(Config.getInteger("CodeGenOptLevel"),
-                                    static_cast<int64_t>(3)),
-        getDefaultValueFromOptional(Config.getInteger("TunedMaxThreads"),
-                                    static_cast<int64_t>(-1L)),
-        getDefaultValueFromOptional(Config.getInteger("MinBlocksPerSM"),
-                                    static_cast<int64_t>(0L)));
-  }
+  CodegenOption codeGenOption() const;
+  bool specializeArgs() const;
+  bool specializeDims() const;
+  bool specializeDimsRange() const;
+  bool specializeLaunchBounds() const;
+  char optLevel() const;
+  int codeGenOptLevel() const;
+  std::optional<const std::string> optPipeline() const;
 
-  CodegenOption codeGenOption() const { return ProteusCodegen; }
-  bool specializeArgs() const { return ProteusSpecializeArgs; }
-  bool specializeDims() const { return ProteusSpecializeDims; }
-  bool specializeDimsRange() const { return ProteusSpecializeDimsRange; }
-  bool specializeLaunchBounds() const { return ProteusSpecializeLaunchBounds; }
-  char optLevel() const { return ProteusOptLevel; }
-  int codeGenOptLevel() const { return ProteusCodeGenOptLevel; }
-  std::optional<const std::string> optPipeline() const {
-    return ProteusOptPipeline;
-  }
-
-  int minBlocksPerSM(int MaxThreads) const {
-    // NOTE: We only return the tuned value when the current LBMaxThreads is
-    // equal to the tuned one. otherwise we return 0. Not doing so, may result
-    // in violating constraints in cases in which LBMaxThreads >
-    // TunedMaxThreads
-    if (TunedMaxThreads != MaxThreads)
-      return 0;
-    return MinBlocksPerSM;
-  }
+  int minBlocksPerSM(int MaxThreads) const;
 
   template <typename T> void dump(T &OS) const {
     if (ProteusOptPipeline)
@@ -285,10 +222,7 @@ parseJSONConfig(std::optional<std::string> JSONFn) {
 
 class Config {
 public:
-  static Config &get() {
-    static Config Conf;
-    return Conf;
-  }
+  static Config &get();
   const CodeGenerationConfig GlobalCodeGenConfig;
   const llvm::StringMap<const CodeGenerationConfig> TunedConfigs;
   bool ProteusUseStoredCache;
@@ -304,59 +238,12 @@ public:
   bool ProteusDebugOutput;
   std::optional<const std::string> ProteusCacheDir;
 
-  const CodeGenerationConfig &getCGConfig(llvm::StringRef KName = "") const {
+  const CodeGenerationConfig &getCGConfig(llvm::StringRef KName = "") const;
 
-    if (TunedConfigs.empty())
-      return GlobalCodeGenConfig;
-
-    if (auto It = TunedConfigs.find(KName); It != TunedConfigs.end())
-      return It->second;
-
-    return GlobalCodeGenConfig;
-  }
-
-  void dump(llvm::raw_ostream &OS) const {
-    auto PrintOut = [](llvm::StringRef ID,
-                       const CodeGenerationConfig &KConfig) {
-      llvm::SmallString<128> S;
-      llvm::raw_svector_ostream OS(S);
-      OS << "ID:" << ID << " ";
-      KConfig.dump(OS);
-      return S;
-    };
-
-    OS << "PROTEUS_USE_STORED_CACHE " << ProteusUseStoredCache << "\n";
-    OS << "PROTEUS_CACHE_DIR " << Config::get().ProteusCacheDir << "\n";
-
-    OS << PrintOut("Default", GlobalCodeGenConfig) << "\n";
-    for (auto &KV : TunedConfigs) {
-      OS << PrintOut(KV.getKey(), KV.second) << "\n";
-    }
-  }
+  void dump(llvm::raw_ostream &OS) const;
 
 private:
-  Config()
-      : GlobalCodeGenConfig(CodeGenerationConfig::createFromEnv()),
-        TunedConfigs(
-            parseJSONConfig(getEnvOrDefaultString("PROTEUS_TUNED_KERNELS"))),
-        ProteusCacheDir(getEnvOrDefaultString("PROTEUS_CACHE_DIR")) {
-    ProteusUseStoredCache =
-        getEnvOrDefaultBool("PROTEUS_USE_STORED_CACHE", true);
-    ProteusDisable = getEnvOrDefaultBool("PROTEUS_DISABLE", false);
-    ProteusDumpLLVMIR = getEnvOrDefaultBool("PROTEUS_DUMP_LLVM_IR", false);
-    ProteusRelinkGlobalsByCopy =
-        getEnvOrDefaultBool("PROTEUS_RELINK_GLOBALS_BY_COPY", false);
-    ProteusAsyncCompilation =
-        getEnvOrDefaultBool("PROTEUS_ASYNC_COMPILATION", false);
-    ProteusAsyncTestBlocking =
-        getEnvOrDefaultBool("PROTEUS_ASYNC_TEST_BLOCKING", false);
-    ProteusAsyncThreads = getEnvOrDefaultInt("PROTEUS_ASYNC_THREADS", 1);
-    ProteusKernelClone = getEnvOrDefaultKC("PROTEUS_KERNEL_CLONE",
-                                           KernelCloneOption::CrossClone);
-    ProteusEnableTimers = getEnvOrDefaultBool("PROTEUS_ENABLE_TIMERS", false);
-    ProteusTraceOutput = getEnvOrDefaultInt("PROTEUS_TRACE_OUTPUT", 0);
-    ProteusDebugOutput = getEnvOrDefaultBool("PROTEUS_DEBUG_OUTPUT", false);
-  }
+  Config();
 };
 } // namespace proteus
 
