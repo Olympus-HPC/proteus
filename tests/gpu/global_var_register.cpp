5a7bd7b89e56f9f5e98e5948cc2e05c4001504b8
koparasy <parasyris1@llnl.gov>
2025-11-12 14:50:48 -0800

    Var naming

--- tests/gpu/global_var_register.cpp
+++ tests/gpu/global_var_register.cpp
@@ -17,22 +17,22 @@ struct SmallStruct {
   char Blob[3];
 };
 
-__device__ char charVar;
-__device__ int intVar;
-__device__ long longVar;
-__device__ float floatVar;
-__device__ double doubleVar;
-__device__ LargeStruct lsVar;
-__device__ SmallStruct ssVar;
+__device__ char CharVar;
+__device__ int IntVar;
+__device__ long LongVar;
+__device__ float FloatVar;
+__device__ double DoubleVar;
+__device__ LargeStruct LSVar;
+__device__ SmallStruct SSVar;
 
 __global__ __attribute__((annotate("jit"))) void kernel() {
-  printf("charVar has size of %ld\n", sizeof(charVar));
-  printf("intVar has size of %ld\n", sizeof(intVar));
-  printf("longVar has size of %ld\n", sizeof(longVar));
-  printf("floatVar has size of %ld\n", sizeof(floatVar));
-  printf("doubleVar has size of %ld\n", sizeof(doubleVar));
-  printf("lsVar has size of %ld\n", sizeof(lsVar));
-  printf("ssVar has size of %ld\n", sizeof(ssVar));
+  printf("CharVar has size of %ld\n", sizeof(CharVar));
+  printf("IntVar has size of %ld\n", sizeof(IntVar));
+  printf("LongVar has size of %ld\n", sizeof(LongVar));
+  printf("FloatVar has size of %ld\n", sizeof(FloatVar));
+  printf("DoubleVar has size of %ld\n", sizeof(DoubleVar));
+  printf("LSVar has size of %ld\n", sizeof(LSVar));
+  printf("SSVar has size of %ld\n", sizeof(SSVar));
 }
 
 int main() {
@@ -46,30 +46,30 @@ int main() {
 }
 
 // clang-format off
-// charVar
-// CHECK-DAG: [GVarInfo]: charVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_CHAR:[0-9]+]]
-// CHECK-DAG: charVar has size of [[SZ_CHAR]]
+// CharVar
+// CHECK-DAG: [GVarInfo]: CharVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_CHAR:[0-9]+]]
+// CHECK-DAG: CharVar has size of [[SZ_CHAR]]
 
-// intVar
-// CHECK-DAG: [GVarInfo]: intVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_INT:[0-9]+]]
-// CHECK-DAG: intVar has size of [[SZ_INT]]
+// IntVar
+// CHECK-DAG: [GVarInfo]: IntVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_INT:[0-9]+]]
+// CHECK-DAG: IntVar has size of [[SZ_INT]]
 
-// longVar
-// CHECK-DAG: [GVarInfo]: longVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_LONG:[0-9]+]] 
-// CHECK-DAG: longVar has size of [[SZ_LONG]]
+// LongVar
+// CHECK-DAG: [GVarInfo]: LongVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_LONG:[0-9]+]] 
+// CHECK-DAG: LongVar has size of [[SZ_LONG]]
 
-// floatVar
-// CHECK-DAG: [GVarInfo]: floatVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_FLOAT:[0-9]+]] 
-// CHECK-DAG: floatVar has size of [[SZ_FLOAT]]
+// FloatVar
+// CHECK-DAG: [GVarInfo]: FloatVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_FLOAT:[0-9]+]] 
+// CHECK-DAG: FloatVar has size of [[SZ_FLOAT]]
 
-// doubleVar
-// CHECK-DAG: [GVarInfo]: doubleVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_DOUBLE:[0-9]+]] 
-// CHECK-DAG: doubleVar has size of [[SZ_DOUBLE]]
+// DoubleVar
+// CHECK-DAG: [GVarInfo]: DoubleVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_DOUBLE:[0-9]+]] 
+// CHECK-DAG: DoubleVar has size of [[SZ_DOUBLE]]
 
-// lsVar
-// CHECK-DAG: [GVarInfo]: lsVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_LS:[0-9]+]] 
-// CHECK-DAG: lsVar has size of [[SZ_LS]]
+// LSVar
+// CHECK-DAG: [GVarInfo]: LSVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_LS:[0-9]+]] 
+// CHECK-DAG: LSVar has size of [[SZ_LS]]
 
-// ssVar
-// CHECK-DAG: [GVarInfo]: ssVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_SS:[0-9]+]] 
-// CHECK-DAG: ssVar has size of [[SZ_SS]]
+// SSVar
+// CHECK-DAG: [GVarInfo]: SSVar HAddr:{{0x[0-9a-f]+}} DevAddr:{{0x[0-9a-f]+}} VarSize:[[SZ_SS:[0-9]+]] 
+// CHECK-DAG: SSVar has size of [[SZ_SS]]
