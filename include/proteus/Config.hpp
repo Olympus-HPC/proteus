1c8d7de7c719833dc20bdc6a7a0b5a293e4decc7
Zane Fink <finkzane@gmail.com>
2025-12-03 17:50:35 -0800

    make trace output configurable

--- include/proteus/Config.hpp
+++ include/proteus/Config.hpp
@@ -314,6 +314,7 @@ public:
   bool ProteusDebugOutput;
   std::optional<const std::string> ProteusCacheDir;
   std::string ProteusObjectCacheChain;
+  std::optional<std::string> ProteusTimeTraceFile;
 
   const CodeGenerationConfig &getCGConfig(llvm::StringRef KName = "") const {
 
@@ -369,6 +370,7 @@ private:
     ProteusDebugOutput = getEnvOrDefaultBool("PROTEUS_DEBUG_OUTPUT", false);
     ProteusObjectCacheChain =
         getEnvOrDefaultString("PROTEUS_OBJECT_CACHE_CHAIN").value_or("storage");
+    ProteusTimeTraceFile = getEnvOrDefaultString("PROTEUS_TIME_TRACE_FILE");
   }
 };
 } // namespace proteus
