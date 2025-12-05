e47200279f69bc673e27cc611ddda0fc28e08498
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-12-04 23:56:04 -0800

    Fixup: find LLD package only for HIP and simplify Clang, LLD search

--- cmake/SetupLLVM.cmake
+++ cmake/SetupLLVM.cmake
@@ -2,40 +2,23 @@
 # The llvm/lib* suffixes are to support ROCm/LLVM installations.
 find_package(LLVM REQUIRED CONFIG NO_DEFAULT_PATH
   HINTS "${LLVM_INSTALL_DIR}"
-  PATH_SUFFIXES
-    "lib/cmake/llvm"
-    "lib64/cmake/llvm"
-    "cmake/llvm"
-    "llvm/lib/cmake/llvm"
-    "llvm/lib64/cmake/llvm"
 )
+message(STATUS "Found LLVM package in: ${LLVM_DIR}")
+message(STATUS "LLVM Version: ${LLVM_VERSION}")
 
 find_package(Clang REQUIRED CONFIG NO_DEFAULT_PATH
-  HINTS "${LLVM_INSTALL_DIR}"
-  PATH_SUFFIXES
-    "lib/cmake/clang"
-    "lib64/cmake/clang"
-    "cmake/clang"
-    "llvm/lib/cmake/clang"
-    "llvm/lib64/cmake/clang"
+  HINTS "${LLVM_INSTALL_PREFIX}"
 )
-
-find_package(LLD REQUIRED CONFIG NO_DEFAULT_PATH
-HINTS "${LLVM_INSTALL_DIR}"
-PATH_SUFFIXES
-  "lib/cmake/lld"
-  "lib64/cmake/lld"
-  "cmake/lld"
-  "llvm/lib/cmake/lld"
-  "llvm/lib64/cmake/lld"
-)
-
-message(STATUS "Found LLVM package in: ${LLVM_DIR}")
 message(STATUS "Found Clang package in: ${Clang_DIR}")
-message(STATUS "Found LLD package in: ${LLD_DIR}")
-message(STATUS "LLVM Version: ${LLVM_VERSION}")
 message(STATUS "Clang Version: ${Clang_VERSION}")
-message(STATUS "LLD Version: ${LLD_VERSION}")
+
+if(PROTEUS_ENABLE_HIP)
+  find_package(LLD REQUIRED CONFIG NO_DEFAULT_PATH
+  HINTS "${LLVM_INSTALL_PREFIX}"
+  )
+  message(STATUS "Found LLD package in: ${LLD_DIR}")
+  message(STATUS "LLD Version: ${LLD_VERSION}")
+endif()
 
 if(NOT LLVM_ENABLE_RTTI)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-rtti")
