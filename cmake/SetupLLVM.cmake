02a6c28c7bf32e955187f4fddb898cb62b1de546
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-12-04 23:43:31 -0800

    Update proteus cmake config to match dependencies by version
    - Move LLD find_package in SetupLLVM.cmake to make it accessible at
    parent scope
    - Use LLVM, Clang, LLD versions instead of dirs for checking
    compatibility; we use EXACT versions which could be relaxed, dirs are
    error-prone and incompatible with packaging

--- cmake/SetupLLVM.cmake
+++ cmake/SetupLLVM.cmake
@@ -20,13 +20,22 @@ find_package(Clang REQUIRED CONFIG NO_DEFAULT_PATH
     "llvm/lib64/cmake/clang"
 )
 
-# Canonicalize found paths when LLVM is built with symlinks to have valid error
-# checking string comparisons.
-file(REAL_PATH "${LLVM_DIR}" LLVM_DIR)
-file(REAL_PATH "${Clang_DIR}" Clang_DIR)
+find_package(LLD REQUIRED CONFIG NO_DEFAULT_PATH
+HINTS "${LLVM_INSTALL_DIR}"
+PATH_SUFFIXES
+  "lib/cmake/lld"
+  "lib64/cmake/lld"
+  "cmake/lld"
+  "llvm/lib/cmake/lld"
+  "llvm/lib64/cmake/lld"
+)
 
 message(STATUS "Found LLVM package in: ${LLVM_DIR}")
 message(STATUS "Found Clang package in: ${Clang_DIR}")
+message(STATUS "Found LLD package in: ${LLD_DIR}")
+message(STATUS "LLVM Version: ${LLVM_VERSION}")
+message(STATUS "Clang Version: ${Clang_VERSION}")
+message(STATUS "LLD Version: ${LLD_VERSION}")
 
 if(NOT LLVM_ENABLE_RTTI)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-rtti")
