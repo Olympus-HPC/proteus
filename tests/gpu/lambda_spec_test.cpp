68589345bb1fb88cf4b97fedf9664dc5faee0550
john bowen <bowen36@llnl.gov>
2026-02-03 10:35:57 -0800

    init

---
+++ tests/gpu/lambda_spec_test.cpp
@@ -0,0 +1,61 @@
+// clang-format off
+// RUN: rm -rf "%t.$$.proteus"
+// RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" PROTEUS_TRACE_OUTPUT=1 %build/indirect_launcher_arg.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-FIRST
+// Second run uses the object cache.
+// RUN: PROTEUS_CACHE_DIR="%t.$$.proteus" %build/indirect_launcher_arg.%ext | %FILECHECK %s --check-prefixes=CHECK,CHECK-SECOND
+// RUN: rm -rf "%t.$$.proteus"
+// clang-format on
+
+#include <climits>
+#include <cstdio>
+
+#include "gpu_common.h"
+#include <proteus/JitInterface.h>
+  
+__device__ void printInt(int I) {
+  printf("Integer = %d\n", I);
+}
+
+template <typename T>
+__global__ __attribute__((annotate("jit"))) void kernel(T LB) {
+  LB();
+}
+
+template <typename T> void run(T &&LB) {
+  proteus::register_lambda(LB);
+  kernel<<<1, 1>>>(LB);
+  gpuErrCheck(gpuDeviceSynchronize());
+}
+
+int main() {
+  int zero = 0;
+  int one = 1;
+  int two = 2;
+  
+  auto zero_lambda = [ =, c = proteus::jit_variable(zero) ] __device__()
+      __attribute__((annotate("jit"))) {
+    printInt(c);
+  };
+  
+  auto one_lambda = [ =, c = proteus::jit_variable(one) ] __device__()
+  __attribute__((annotate("jit"))) {
+    printInt(c);
+  };
+  
+  auto two_lambda = [ =, c = proteus::jit_variable(two) ] __device__()
+  __attribute__((annotate("jit"))) {
+    printInt(c);
+  };
+  
+  run(zero_lambda);
+  run(one_lambda);
+  run(two_lambda);
+  
+  return 0;
+}
+
+// clang-format off
+// CHECK-FIRST: [LambdaSpec] Replacing slot 0 with i32 0
+// CHECK Integer = 0
+// CHECK-FIRST: [LambdaSpec] Replacing slot 0 with i32 1
+// CHECK Integer = 1
