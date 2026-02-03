528ef6ae929312f6172e2cea719786f306f3367b
Konstantinos Parasyris <parasyris1@llnl.gov>
2026-02-02 21:25:26 -0800

    Fix LLVM static initialization with shared proteus (#374)
    * Add integration test that links LLVM and shared proteus
    
    * Hide LLVM symbols in proteus
    
    - Use version script when linking to export only proteus symbols
    - Add exports.map file
    
    ---------
    
    Co-authored-by: Daniel Nichols <dando18studios@gmail.com>
    Co-authored-by: Giorgis Georgakoudis <georgakoudis1@llnl.gov>

---
+++ tests/integration/hip/cmake-link_llvm-shared_proteus/ci-build-and-run.sh
@@ -0,0 +1,25 @@
+set -e
+
+cmake -S ${CI_PROJECT_DIR} -B build-proteus \
+    -DLLVM_INSTALL_DIR="${LLVM_INSTALL_DIR}" \
+    -DCMAKE_C_COMPILER="${LLVM_INSTALL_DIR}/bin/clang" \
+    -DCMAKE_CXX_COMPILER="${LLVM_INSTALL_DIR}/bin/clang++" \
+    -DCMAKE_INSTALL_PREFIX=${PWD}/install-proteus \
+    -DPROTEUS_ENABLE_HIP=on \
+    -DENABLE_TESTS=off \
+    -DBUILD_SHARED=on
+
+pushd build-proteus
+make -j install
+popd
+
+cmake -S ${TEST_DIR} -B build \
+    -Dproteus_DIR="${PWD}/install-proteus" \
+    -DCMAKE_INSTALL_PREFIX="${PWD}/install"
+pushd build
+make -j install
+./main
+popd
+
+# Run from install directory.
+install/bin/main
