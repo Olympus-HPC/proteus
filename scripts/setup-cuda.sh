6b75a12b427f1b669d3c75cd3f2f16274d3ee240
Zane Fink <finkzane@gmail.com>
2026-02-10 12:59:38 -0800

    Add install prefix and hostname to CUDA setup script (#401)

--- scripts/setup-cuda.sh
+++ scripts/setup-cuda.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+HOSTN=${HOSTNAME//[0-9]/}
+
 ml load cmake/3.23.1
 
 LLVM_INSTALL_DIR=$1
@@ -14,8 +16,15 @@ ml load cuda/${CUDA_VERSION}
 
 export PATH="$LLVM_INSTALL_DIR/bin":$PATH
 
-mkdir build-cuda-${CUDA_VERSION}
-pushd build-cuda-${CUDA_VERSION}
+LLVM_VERSION=$("$LLVM_INSTALL_DIR/bin/llvm-config" --version)
+if [ -z "${LLVM_VERSION}" ]; then
+    echo "Error: could not determine LLVM version from $LLVM_INSTALL_DIR/bin/llvm-config"
+    return 1
+fi
+
+BUILDDIR="build-${HOSTN}-cuda-${CUDA_VERSION}-llvm-${LLVM_VERSION}"
+mkdir "${BUILDDIR}"
+pushd "${BUILDDIR}"
 
 cmake .. \
 -DLLVM_INSTALL_DIR="$LLVM_INSTALL_DIR" \
@@ -24,6 +33,7 @@ cmake .. \
 -DCMAKE_C_COMPILER="$LLVM_INSTALL_DIR/bin/clang" \
 -DCMAKE_CXX_COMPILER="$LLVM_INSTALL_DIR/bin/clang++" \
 -DCMAKE_CUDA_COMPILER="$LLVM_INSTALL_DIR/bin/clang++" \
+-DCMAKE_INSTALL_PREFIX=../install-${HOSTN}-cuda-${CUDA_VERSION}-llvm-${LLVM_VERSION} \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=on \
 "${@:3}"
 
