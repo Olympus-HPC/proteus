84dca5d91a6f7b08b4d1f912f7bff93a35f1ed8e
john bowen <bowen36@llnl.gov>
2026-03-16 14:35:23 -0700

    Update test intrinsic names

--- tests/gpu/dim_spec_cache_test.cpp
+++ tests/gpu/dim_spec_cache_test.cpp
@@ -53,21 +53,21 @@ int main() {
 }
 
 // clang-format off
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_xv with constant i32 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_yv with constant i32 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_zv with constant i32 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_xv with constant i32 2
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_yv with constant i32 2
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_zv with constant i32 2
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.x [0,2)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.y [0,2)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.z [0,2)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.x [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.x [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.y [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.y [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.z [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.z [0,4)
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__XcvjEv|_ZL20__hip_get_grid_dim_xv|llvm.nvvm.read.ptx.sreg.nctaid.x}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__YcvjEv|_ZL20__hip_get_grid_dim_yv|llvm.nvvm.read.ptx.sreg.nctaid.y}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__ZcvjEv|_ZL20__hip_get_grid_dim_zv|llvm.nvvm.read.ptx.sreg.nctaid.z}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__XcvjEv|_ZL21__hip_get_block_dim_xv|llvm.nvvm.read.ptx.sreg.ntid.x}} with constant i32 2
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__YcvjEv|_ZL21__hip_get_block_dim_yv|llvm.nvvm.read.ptx.sreg.ntid.y}} with constant i32 2
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__ZcvjEv|_ZL21__hip_get_block_dim_zv|llvm.nvvm.read.ptx.sreg.ntid.z}} with constant i32 2
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.x|llvm.nvvm.read.ptx.sreg.tid.x}} [0,2)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.y|llvm.nvvm.read.ptx.sreg.tid.y}} [0,2)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.z|llvm.nvvm.read.ptx.sreg.tid.z}} [0,2)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.x|llvm.nvvm.read.ptx.sreg.ctaid.x}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.x|llvm.nvvm.read.ptx.sreg.ctaid.x}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.y|llvm.nvvm.read.ptx.sreg.ctaid.y}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.y|llvm.nvvm.read.ptx.sreg.ctaid.y}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.z|llvm.nvvm.read.ptx.sreg.ctaid.z}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.z|llvm.nvvm.read.ptx.sreg.ctaid.z}} [0,4)
 // CHECK-FIRST: [LaunchBoundSpec] MaxThreads 8 MinBlocksPerSM 0
 // CHECK-THIRD: [LaunchBoundSpec] MaxThreads 8 MinBlocksPerSM 0
 // CHECK-SECOND: [ObjectCacheChain] Hit at level 0 (Storage) for hash {{[0-9]+}}
@@ -78,21 +78,21 @@ int main() {
 // CHECK: Grid dim.x = 4
 // CHECK: Grid dim.y = 4
 // CHECK: Grid dim.z = 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_xv with constant i32 8
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_yv with constant i32 8
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_zv with constant i32 8
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_xv with constant i32 2
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_yv with constant i32 2
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_zv with constant i32 2
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.x [0,2)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.y [0,2)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.z [0,2)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.x [0,8)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.x [0,8)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.y [0,8)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.y [0,8)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.z [0,8)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.z [0,8)
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__XcvjEv|_ZL20__hip_get_grid_dim_xv|llvm.nvvm.read.ptx.sreg.nctaid.x}} with constant i32 8
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__YcvjEv|_ZL20__hip_get_grid_dim_yv|llvm.nvvm.read.ptx.sreg.nctaid.y}} with constant i32 8
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__ZcvjEv|_ZL20__hip_get_grid_dim_zv|llvm.nvvm.read.ptx.sreg.nctaid.z}} with constant i32 8
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__XcvjEv|_ZL21__hip_get_block_dim_xv|llvm.nvvm.read.ptx.sreg.ntid.x}} with constant i32 2
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__YcvjEv|_ZL21__hip_get_block_dim_yv|llvm.nvvm.read.ptx.sreg.ntid.y}} with constant i32 2
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__ZcvjEv|_ZL21__hip_get_block_dim_zv|llvm.nvvm.read.ptx.sreg.ntid.z}} with constant i32 2
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.x|llvm.nvvm.read.ptx.sreg.tid.x}} [0,2)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.y|llvm.nvvm.read.ptx.sreg.tid.y}} [0,2)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.z|llvm.nvvm.read.ptx.sreg.tid.z}} [0,2)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.x|llvm.nvvm.read.ptx.sreg.ctaid.x}} [0,8)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.x|llvm.nvvm.read.ptx.sreg.ctaid.x}} [0,8)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.y|llvm.nvvm.read.ptx.sreg.ctaid.y}} [0,8)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.y|llvm.nvvm.read.ptx.sreg.ctaid.y}} [0,8)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.z|llvm.nvvm.read.ptx.sreg.ctaid.z}} [0,8)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.z|llvm.nvvm.read.ptx.sreg.ctaid.z}} [0,8)
 // CHECK-THIRD: [LaunchBoundSpec] MaxThreads 8 MinBlocksPerSM 0
 // CHECK-FOURTH: [ObjectCacheChain] Hit at level 0 (Storage) for hash {{[0-9]+}}
 // CHECK: Block dim.x = 2
@@ -101,21 +101,21 @@ int main() {
 // CHECK: Grid dim.x = 8
 // CHECK: Grid dim.y = 8
 // CHECK: Grid dim.z = 8
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_xv with constant i32 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_yv with constant i32 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL20__hip_get_grid_dim_zv with constant i32 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_xv with constant i32 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_yv with constant i32 4
-// CHECK-THIRD: [DimSpec] Replace call to _ZL21__hip_get_block_dim_zv with constant i32 4
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.x [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.y [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workitem.id.z [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.x [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.x [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.y [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.y [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.z [0,4)
-// CHECK-THIRD: [DimSpec] Range llvm.amdgcn.workgroup.id.z [0,4)
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__XcvjEv|_ZL20__hip_get_grid_dim_xv|llvm.nvvm.read.ptx.sreg.nctaid.x}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__YcvjEv|_ZL20__hip_get_grid_dim_yv|llvm.nvvm.read.ptx.sreg.nctaid.y}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI13__HIP_GridDimE3__ZcvjEv|_ZL20__hip_get_grid_dim_zv|llvm.nvvm.read.ptx.sreg.nctaid.z}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__XcvjEv|_ZL21__hip_get_block_dim_xv|llvm.nvvm.read.ptx.sreg.ntid.x}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__YcvjEv|_ZL21__hip_get_block_dim_yv|llvm.nvvm.read.ptx.sreg.ntid.y}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Replace call to {{_ZNK17__HIP_CoordinatesI14__HIP_BlockDimE3__ZcvjEv|_ZL21__hip_get_block_dim_zv|llvm.nvvm.read.ptx.sreg.ntid.z}} with constant i32 4
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.x|llvm.nvvm.read.ptx.sreg.tid.x}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.y|llvm.nvvm.read.ptx.sreg.tid.y}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workitem.id.z|llvm.nvvm.read.ptx.sreg.tid.z}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.x|llvm.nvvm.read.ptx.sreg.ctaid.x}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.x|llvm.nvvm.read.ptx.sreg.ctaid.x}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.y|llvm.nvvm.read.ptx.sreg.ctaid.y}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.y|llvm.nvvm.read.ptx.sreg.ctaid.y}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.z|llvm.nvvm.read.ptx.sreg.ctaid.z}} [0,4)
+// CHECK-THIRD: [DimSpec] Range {{llvm.amdgcn.workgroup.id.z|llvm.nvvm.read.ptx.sreg.ctaid.z}} [0,4)
 // CHECK-FIRST: [LaunchBoundSpec] MaxThreads 64 MinBlocksPerSM 0
 // CHECK-THIRD: [LaunchBoundSpec] MaxThreads 64 MinBlocksPerSM 0
 // CHECK-SECOND: [ObjectCacheChain] Hit at level 0 (Storage) for hash {{[0-9]+}}
