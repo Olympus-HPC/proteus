0d3b2773b253b84b0b2d7c3da67d0b529dfefb1b
john bowen <john.bowen42@gmail.com>
2026-05-27 10:21:46 -0700

    Add CMake method for connecting plugin pass

--- cmake/ProteusFunctions.cmake
+++ cmake/ProteusFunctions.cmake
@@ -28,3 +28,60 @@ function(add_proteus target)
 
     message(STATUS "Linked target '${target}' with libproteus")
 endfunction()
+
+function(proteus_register_jit_pass_plugin target)
+    if(NOT TARGET ${target})
+        message(FATAL_ERROR "Target '${target}' does not exist")
+    endif()
+
+    set(one_value_args PLUGIN_TARGET PLUGIN_PATH PIPELINE)
+    cmake_parse_arguments(PROTEUS_JIT_PASS "" "${one_value_args}" "" ${ARGN})
+
+    if(NOT PROTEUS_JIT_PASS_PIPELINE)
+        message(FATAL_ERROR "proteus_register_jit_pass_plugin requires PIPELINE")
+    endif()
+
+    if(PROTEUS_JIT_PASS_PLUGIN_TARGET AND PROTEUS_JIT_PASS_PLUGIN_PATH)
+        message(FATAL_ERROR
+            "proteus_register_jit_pass_plugin accepts either PLUGIN_TARGET or PLUGIN_PATH, not both")
+    endif()
+
+    if(NOT PROTEUS_JIT_PASS_PLUGIN_TARGET AND NOT PROTEUS_JIT_PASS_PLUGIN_PATH)
+        message(FATAL_ERROR
+            "proteus_register_jit_pass_plugin requires PLUGIN_TARGET or PLUGIN_PATH")
+    endif()
+
+    if(PROTEUS_JIT_PASS_PLUGIN_TARGET)
+        if(NOT TARGET ${PROTEUS_JIT_PASS_PLUGIN_TARGET})
+            message(FATAL_ERROR
+                "Plugin target '${PROTEUS_JIT_PASS_PLUGIN_TARGET}' does not exist")
+        endif()
+        set(_proteus_jit_pass_plugin_path "$<TARGET_FILE:${PROTEUS_JIT_PASS_PLUGIN_TARGET}>")
+        add_dependencies(${target} ${PROTEUS_JIT_PASS_PLUGIN_TARGET})
+    else()
+        set(_proteus_jit_pass_plugin_path "${PROTEUS_JIT_PASS_PLUGIN_PATH}")
+    endif()
+
+    string(MD5 _proteus_jit_pass_key
+        "${target};${_proteus_jit_pass_plugin_path};${PROTEUS_JIT_PASS_PIPELINE}")
+    set(_proteus_jit_pass_source
+        "${CMAKE_CURRENT_BINARY_DIR}/${target}.proteus_jit_pass_${_proteus_jit_pass_key}.cpp")
+
+    file(GENERATE OUTPUT "${_proteus_jit_pass_source}" CONTENT
+"#include <proteus/Init.h>
+
+namespace {
+struct AutoRegisterProteusJITPassPlugin {
+  AutoRegisterProteusJITPassPlugin() {
+    proteus::registerJITPassPlugin(
+        R\"(${_proteus_jit_pass_plugin_path})\",
+        R\"(${PROTEUS_JIT_PASS_PIPELINE})\");
+  }
+};
+
+AutoRegisterProteusJITPassPlugin AutoRegisterProteusJITPassPluginInstance;
+} // namespace
+")
+
+    target_sources(${target} PRIVATE "${_proteus_jit_pass_source}")
+endfunction()
