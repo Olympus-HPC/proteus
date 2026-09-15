70b6248e5389470b8136d88e28f434e37a851300
John Bowen <john.bowen42@gmail.com>
2026-09-15 12:59:48 -0700

    Fix RAJA/MFEM launch after tidy refactor

--- tests/gpu/raja_mfem_style_launch.h
+++ tests/gpu/raja_mfem_style_launch.h
@@ -9,12 +9,12 @@ template <typename DBODY> void rajaWrap(const int N, DBODY &&DBody) {
 
 template <typename d_lambda>
 inline void forallWrap(const int N, d_lambda &&DBody) {
-  return RajaWrap(N, std::forward<d_lambda>(DBody));
+  return rajaWrap(N, std::forward<d_lambda>(DBody));
 }
 
 template <typename lambda>
 __attribute__((noinline)) void forall(int N, lambda &&Body) {
-  ForallWrap(N, std::forward<lambda>(Body));
+  forallWrap(N, std::forward<lambda>(Body));
 }
 
 template <typename lambda>
@@ -22,7 +22,7 @@ __attribute__((noinline)) void forall(int Nx, int Ny, lambda &&Body) {
   MockMfemInterface::forall(Nx * Ny, [=] PROTEUS_HOST_DEVICE(int idx) {
     int j = idx / Nx;
     int i = idx % Nx;
-    body(i, j);
+    Body(i, j);
   });
 }
 } // namespace MockMfemInterface
