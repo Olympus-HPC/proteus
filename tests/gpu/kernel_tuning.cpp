a4d845581d70a522ec323d0ce0127c1af08cb038
John Bowen <31394001+johnbowen42@users.noreply.github.com>
2026-09-17 13:40:08 -0700

    performance: Disable range specialization by default (#524)
    Prior to this commit, the specialization enabled by PROTEUS_SPECIALIZE_DIMS_RANGE was enabled by default for HIP. However, this caused serious performance regressions in MFEM's TMOP kernels, as well as production codes. This PR leaves the specialization but disables it by default for both HIP and CUDA

--- tests/gpu/kernel_tuning.cpp
+++ tests/gpu/kernel_tuning.cpp
@@ -44,14 +44,14 @@ int main() {
 // CHECK-HIP: [KernelConfig] ID:foo Pipeline:default<O3>,globaldce CG:Serial SA:1 LB:0 SD:0 SDR:0 OL:3 CGL:2 TMT:1 BPSM:4
 // CHECK: [CustomPipeline] default<O3>,globaldce
 // CHECK: Hello from foo
-// CHECK: [KernelConfig] ID:bar CG:RTC SA:1 LB:1 SD:1 SDR:1 OL:3 CGL:2 TMT:4 BPSM:4
+// CHECK: [KernelConfig] ID:bar CG:RTC SA:1 LB:1 SD:1 SDR:0 OL:3 CGL:2 TMT:4 BPSM:4
 // CHECK: [LaunchBoundSpec] MaxThreads 1 MinBlocksPerSM 0
 // CHECK: Hello from bar
-// CHECK: [KernelConfig] ID:bar CG:RTC SA:1 LB:1 SD:1 SDR:1 OL:3 CGL:2 TMT:4 BPSM:4
+// CHECK: [KernelConfig] ID:bar CG:RTC SA:1 LB:1 SD:1 SDR:0 OL:3 CGL:2 TMT:4 BPSM:4
 // CHECK: [LaunchBoundSpec] MaxThreads 4 MinBlocksPerSM 4
 // CHECK: Hello from bar
 // CHECK-CUDA: [KernelConfig] ID:baz CG:RTC SA:1 LB:1 SD:1 SDR:0 OL:3 CGL:3 TMT:-1 BPSM:0
-// CHECK-HIP: [KernelConfig] ID:baz CG:{{RTC|Serial|Parallel}} SA:1 LB:1 SD:1 SDR:1 OL:3 CGL:3 TMT:-1 BPSM:0
+// CHECK-HIP: [KernelConfig] ID:baz CG:{{RTC|Serial|Parallel}} SA:1 LB:1 SD:1 SDR:0 OL:3 CGL:3 TMT:-1 BPSM:0
 // CHECK: [LaunchBoundSpec] MaxThreads 1 MinBlocksPerSM 0
 // CHECK: Hello from baz
 // CHECK: [proteus][JitEngineDevice] MemoryCache rank 0 hits 0 accesses 4
